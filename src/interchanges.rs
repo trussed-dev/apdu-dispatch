a6f00115ad5e6fba573c626195186360e733e679
Sosthène Guédon <sosthene@nitrokey.com>
2023-05-02 16:11:32 +0200

    Fix typo: Responder => Requester

--- src/interchanges.rs
+++ src/interchanges.rs
@@ -1,4 +1,4 @@
 pub const SIZE: usize = 3072;
 pub type Data = iso7816::Data<SIZE>;
 pub type Responder<'pipe> = interchange::Responder<'pipe, Data, Data>;
-pub type Requester<'pipe> = interchange::Responder<'pipe, Data, Data>;
+pub type Requester<'pipe> = interchange::Requester<'pipe, Data, Data>;
