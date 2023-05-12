6a2720d06930b715a7af9411a12c7fcf35d5995f
Sosthène Guédon <sosthene@nitrokey.com>
2023-05-12 09:39:17 +0200

    Add Channel helper type

--- src/interchanges.rs
+++ src/interchanges.rs
@@ -2,3 +2,4 @@ pub const SIZE: usize = 3072;
 pub type Data = iso7816::Data<SIZE>;
 pub type Responder<'pipe> = interchange::Responder<'pipe, Data, Data>;
 pub type Requester<'pipe> = interchange::Requester<'pipe, Data, Data>;
+pub type Channel = interchange::Channel<Data, Data>;
